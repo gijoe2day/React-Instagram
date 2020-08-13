08683767927be593c7031d44068613bc8f00dc63
gijoe2day <gjuliao32@hotmail.com>
2020-08-13 09:00:31 -0500

    Updates

---
+++ precache-manifest.ced0eaabed7489bf287cba3ffbbe5e21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf8d461f0773556fc8b4bfdd17fc0e9",
+    "url": "/React-Instagram/index.html"
+  },
+  {
+    "revision": "e3ebd82edfe97c3467ec",
+    "url": "/React-Instagram/static/css/main.b7a8c7d3.chunk.css"
+  },
+  {
+    "revision": "17366da02ff3d58c6f6a",
+    "url": "/React-Instagram/static/js/2.ce96afe1.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/React-Instagram/static/js/2.ce96afe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3ebd82edfe97c3467ec",
+    "url": "/React-Instagram/static/js/main.074113d0.chunk.js"
+  },
+  {
+    "revision": "7b94f615c63df2ac5f2c",
+    "url": "/React-Instagram/static/js/runtime-main.7579bcef.js"
+  }
+]);
